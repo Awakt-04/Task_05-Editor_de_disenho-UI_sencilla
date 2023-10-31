c62e273f972d06992475eee578b58855c9476200
Awakt-04 <asimay898@alu.centroafuera.es>
2023-10-31 09:15:46 +0100

    segundo commit - comentarios

--- app/src/main/java/com/example/task05editordediseo_uisencilla/MainActivity.kt
+++ app/src/main/java/com/example/task05editordediseo_uisencilla/MainActivity.kt
@@ -5,17 +5,17 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-
+// Clase MainActivity
 class MainActivity : AppCompatActivity() {
-
+// Variable de inicio tardío de tipo Button
     private lateinit var btBoton1: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+// Busca el id del botón
         btBoton1 = findViewById(R.id.button)
-
+// al pulsar el botón cambia color de fondo y texto (del botón)
         btBoton1.setOnClickListener{
             btBoton1.setBackgroundColor(Color.BLACK)
             btBoton1.setTextColor(Color.WHITE)
